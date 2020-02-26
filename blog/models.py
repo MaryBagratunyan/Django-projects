3dc15379dac7db0d899fdaba1fa2301c5a6b62e9
marybagratunyan <mary.bagratunyan@gmail.com>
2020-02-26 16:23:19 +0400

    Created blog app with models Post and Comment

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=40)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=70)
+    website = models.URLField(max_length=200, null=True, blank=True)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
