7ae6a0d10f12b63bec1ab60a88ba2c1a57c9278f
marybagratunyan <mary.bagratunyan@gmail.com>
2020-03-09 15:31:55 +0400

    Added templates for wiki

--- wiki/urls.py
+++ wiki/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', ArticleList.as_view(), name='wiki_article_list'),
     path('add/article', add_article, name='wiki_article_add'),
     path('edit/article/<str:slug>', edit_article, name='wiki_article_edit'),
-    path('detail/article/<str:slug>', ArticleDetail.as_view(), name='wiki_article_history'),
+    path('detail/article/<str:slug>', ArticleDetail.as_view(), name='wiki_article_detail'),
     path('history/article/<str:slug>', article_history, name='wiki_article_history')
 ]
