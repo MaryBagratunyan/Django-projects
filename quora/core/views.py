23649853959a7ece7361ca17d7fd0c845a774df5
marybagratunyan <mary.bagratunyan@gmail.com>
2020-03-12 15:13:40 +0400

    Added views, templates and urls, Register page is working

--- quora/core/views.py
+++ quora/core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormView
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+from .forms import RegisterForm
+
+
+class RegisterView(FormView):
+    def get(self, request):
+        return render(request, 'core/register.html', {'form': RegisterForm})
+
+    def post(self, request):
+        form = RegisterForm(request.POST or None)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user['password'] = make_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect(reverse('dashboard-view'))
+        return render(request, 'core/register.html', {'form': RegisterForm})
+
